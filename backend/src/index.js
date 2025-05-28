8c3eb119243b9370c6eff847d231da80a8902ff1
impappdavid <impappdavid@gmail.com>
2025-05-29 00:15:45 +0200

    Adding middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,14 +9,29 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(json({limit: '100mb'}))
-app.use(urlencoded({limit: '100mb', extended: true}))
+const corsOption = {
+    origin: ['http://localhost:3001'],
+    creadentials: true,
+}
+app.use(cors(corsOption))
+app.use(json({ limit: '100mb' }))
+app.use(urlencoded({ limit: '100mb', extended: true }))
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    }
+}))
+app.use(passport.initialize());
+app.use(passport.session())
 
 // Routes
 
 
 // Listen app
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
